a2490dff26679a0ebc3e0023e405058421c53376
Emirhan Soylu <61618968+EmirhanSyl@users.noreply.github.com>
2025-08-23 19:50:56 +0300

    Update api.tsx

--- src/app/lib/api.tsx
+++ src/app/lib/api.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://93.183.74.124/:8000/api/',
+    baseURL: 'http://93.183.74.124:8000/api/',
     // headers, interceptors vb. ekleyebilirsiniz
 });
 
